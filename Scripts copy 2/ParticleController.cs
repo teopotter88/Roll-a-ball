19647dce11aff0bee00f52ba5483a2d4dec4c299
teopotter88 <teoman.ozkan@ggl.hisarschool.k12.tr>
2018-01-20 14:46:44 +0300

    scripts and game photo

---
+++ Scripts copy 2/ParticleController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleController : MonoBehaviour {
+
+	public GameObject pickUp;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - pickUp.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = pickUp.transform.position + offset;
+	}
+}
